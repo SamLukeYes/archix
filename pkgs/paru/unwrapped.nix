2b0e33ef81649142fc602f2bb35e9002639b1cb9
SamLukeYes <samlukeyes123@gmail.com>
2023-12-02 16:40:54 +0800

    paru: 2.0.0 -> 2.0.1

--- pkgs/paru/unwrapped.nix
+++ pkgs/paru/unwrapped.nix
@@ -10,19 +10,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "paru-unwrapped";
-  version = "2.0.0";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "Morganamilo";
     repo = "paru";
     rev = "v${version}";
-    hash = "sha256-XruKt+Y69HPN3qq3oz/faTfWXVLMmc+DpEz4xkysJqU=";
+    hash = "sha256-lCPrhSpH+ounBNm1hpr0dIimFQ/6ozJJfP/TXzkLmac=";
   };
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
-      "alpm-2.2.2" = "sha256-az1v0auvqOzyhOqybHylKM8mb62SjNrBiEHZ/ozfgXA=";
+      "alpm-2.2.2" = "sha256-khUKX3OqNC6vpeSU0+IW9inwtnyARZVi+InFP4Lckz8=";
       "aur-depends-3.0.0" = "sha256-mvWuZ3FfDWwcijCCbuqNjP4mp/BbUCsHWwuVeIEUvOU=";
     };
   };
