b33c5a74b992eb045a9e3463333ea55747639697
SamLukeYes <samlukeyes123@gmail.com>
2024-03-07 14:07:51 +0800

    paru: build against libalpm 13

--- default.nix
+++ default.nix
@@ -4,8 +4,16 @@ with pkgs; rec {
   archlinux-keyring = callPackage ./pkgs/archlinux-keyring { };
   devtools = callPackage ./pkgs/devtools { };
   devtools-riscv64 = callPackage ./pkgs/devtools { enableRiscV = true; };
-  paru-unwrapped = callPackage ./pkgs/paru/unwrapped.nix { };
+  paru-unwrapped = callPackage ./pkgs/paru/unwrapped.nix {
+    pacman = pacman.overrideAttrs (old: rec {
+      version = "6.0.2";    # libalpm 13
+      src = fetchurl {
+        url = "https://sources.archlinux.org/other/${old.pname}/${old.pname}-${version}.tar.xz";
+        hash = "sha256-fY4+jFEhrsCWXfcfWb7fRgUsbPFPljZcRBHsPeCkwaU=";
+      };
+    });
+  };
   paru = callPackage ./pkgs/paru {
-    inherit  devtools paru-unwrapped;
+    inherit devtools paru-unwrapped;
   };
 }
