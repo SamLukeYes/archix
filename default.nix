a672fc38ce37a35248f15fd0233f827cd6501d00
SamLukeYes <samlukeyes123@gmail.com>
2024-03-14 09:37:37 +0800

    paru: 2.0.1 -> 2.0.3

--- default.nix
+++ default.nix
@@ -4,15 +4,7 @@ with pkgs; rec {
   archlinux-keyring = callPackage ./pkgs/archlinux-keyring { };
   devtools = callPackage ./pkgs/devtools { };
   devtools-riscv64 = callPackage ./pkgs/devtools { enableRiscV = true; };
-  paru-unwrapped = callPackage ./pkgs/paru/unwrapped.nix {
-    pacman = pacman.overrideAttrs (old: rec {
-      version = "6.0.2";    # libalpm 13
-      src = fetchurl {
-        url = "https://sources.archlinux.org/other/${old.pname}/${old.pname}-${version}.tar.xz";
-        hash = "sha256-fY4+jFEhrsCWXfcfWb7fRgUsbPFPljZcRBHsPeCkwaU=";
-      };
-    });
-  };
+  paru-unwrapped = callPackage ./pkgs/paru/unwrapped.nix { };
   paru = callPackage ./pkgs/paru {
     inherit devtools paru-unwrapped;
   };
--- pkgs/paru/unwrapped.nix
+++ pkgs/paru/unwrapped.nix
@@ -10,20 +10,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "paru-unwrapped";
-  version = "2.0.1";
+  version = "2.0.3";
 
   src = fetchFromGitHub {
     owner = "Morganamilo";
     repo = "paru";
     rev = "v${version}";
-    hash = "sha256-lCPrhSpH+ounBNm1hpr0dIimFQ/6ozJJfP/TXzkLmac=";
+    hash = "sha256-0+N1WkjHd2DREoS1pImXXvlJ3wXoXEBxFBtupjXqyP8=";
   };
 
   cargoLock = {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
-      "alpm-2.2.2" = "sha256-khUKX3OqNC6vpeSU0+IW9inwtnyARZVi+InFP4Lckz8=";
-      "aur-depends-3.0.0" = "sha256-mvWuZ3FfDWwcijCCbuqNjP4mp/BbUCsHWwuVeIEUvOU=";
+      "alpm-3.0.4" = "sha256-cfIOCUyb+kDAT3Bn50oKuJzIyMyeFyOPBFQMkAgMocI=";
+      "aur-depends-3.0.0" = "sha256-Z/vCd4g3ic29vC0DXFHTT167xFAXYxzO2YQc0XQOerE=";
     };
   };
 
