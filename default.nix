4c64aaf234ddc800f1d54be2c641f644ebcf42f7
SamLukeYes <samlukeyes123@gmail.com>
2024-08-31 00:37:22 +0800

    paru: remove

--- default.nix
+++ default.nix
@@ -1,11 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs; rec {
+with pkgs; {
   archlinux-keyring = callPackage ./pkgs/archlinux-keyring { };
   devtools = callPackage ./pkgs/devtools { };
   devtools-riscv64 = callPackage ./pkgs/devtools { enableRiscV = true; };
-  paru-unwrapped = callPackage ./pkgs/paru/unwrapped.nix { };
-  paru = callPackage ./pkgs/paru {
-    inherit devtools paru-unwrapped;
-  };
 }
