7a744895c3ef2400f9ac4b00d45d580979328f3c
SamLukeYes <samlukeyes123@gmail.com>
2025-04-30 21:58:43 +0800

    archlinux-keyring: 20250123.1 -> 20250430

--- pkgs/archlinux-keyring/default.nix
+++ pkgs/archlinux-keyring/default.nix
@@ -5,38 +5,19 @@
 , sequoia-sq
 }:
 
-let
-  sequoia-sq' = sequoia-sq.overrideAttrs (oldAttrs: rec {
-    version = "0.39.0";
-    src = fetchFromGitLab {
-      owner = "sequoia-pgp";
-      repo = "sequoia-sq";
-      rev = "v${version}";
-      hash = "sha256-nLrwf/4vbASOAyOWlc4X9ZQKFq/Kdh83XadpRuquEA4=";
-    };
-
-    # https://discourse.nixos.org/t/how-do-you-override-the-commit-rev-used-by-a-rust-package/47698/6
-    cargoDeps = oldAttrs.cargoDeps.overrideAttrs {
-      inherit src;
-      name = "${oldAttrs.pname}-${version}-vendor.tar.gz";
-      outputHash = "sha256-MnxvuO1KG7X2plFkQ/DNBHnH2cPi1X3SVbvcN8N7ZXk=";
-    };
-  });
-in
-
 stdenvNoCC.mkDerivation rec {
   pname = "archlinux-keyring";
-  version = "20250123.1";
+  version = "20250430";
 
   src = fetchFromGitLab {
     domain = "gitlab.archlinux.org";
     owner = "archlinux";
     repo = pname;
     rev = version;
-    hash = "sha256-XysXHAeefpY35L2UV4W8MtcEX5Do/FwBNO8J/2CNOiQ=";
+    hash = "sha256-+7gVql5nSQI7hZXi7mW7KxuYSuyUcbwQU3ubFAFKM4s=";
   };
 
-  nativeBuildInputs = [ python3 sequoia-sq' ];
+  nativeBuildInputs = [ python3 sequoia-sq ];
 
   makeFlags = [ "PREFIX=$(out)" ];
 
