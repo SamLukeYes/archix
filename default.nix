5016cd2bb398e2db7c02da513bc81e98d600eadc
SamLukeYes <samlukeyes123@gmail.com>
2025-05-06 22:39:22 +0800

    treewide: refactor with lib.packagesFromDirectoryRecursive

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs; {
-  archlinux-keyring = callPackage ./pkgs/archlinux-keyring { };
-  devtools = callPackage ./pkgs/devtools { };
+with pkgs;
+lib.packagesFromDirectoryRecursive {
+  inherit callPackage;
+  directory = ./pkgs;
 }
