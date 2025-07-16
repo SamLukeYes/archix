46e72275db40dc3ec76ee1a664c23373956b1ec7
SamLukeYes <samlukeyes123@gmail.com>
2025-07-17 07:00:55 +0800

    archlinux-keyring: 20250707 -> 20250716

--- pkgs/archlinux-keyring/package.nix
+++ pkgs/archlinux-keyring/package.nix
@@ -7,14 +7,14 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "archlinux-keyring";
-  version = "20250707";
+  version = "20250716";
 
   src = fetchFromGitLab {
     domain = "gitlab.archlinux.org";
     owner = "archlinux";
     repo = pname;
     rev = version;
-    hash = "sha256-sD5y15pcLkj9oS6a68dluDndhfbp2IscIt/ZeKcvQdA=";
+    hash = "sha256-PMo352CzjJGlAca+49fj/Hf6b81WIj/Op025eNp6ghU=";
   };
 
   nativeBuildInputs = [ python3 sequoia-sq ];
